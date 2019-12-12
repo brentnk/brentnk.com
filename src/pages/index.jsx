0a5abaeb46e2303acfe2b906527142ad7cc27007
Brent Nkomo <brent@choozle.com>
2019-12-12 09:30:27 -0700

    Add basic link on homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
+import { Link } from "gatsby"
 
 export default () => (
   <Layout>
-    
+    <Link to={`/resume`}>Resume</Link>
   </Layout>
 )
